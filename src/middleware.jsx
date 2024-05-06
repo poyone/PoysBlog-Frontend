88a90f234585b01994a0e6b0506fbc4bac7ab612
poyone <86655640+poyone@users.noreply.github.com>
2024-05-06 21:43:50 +0800

    增加中间件的保护路由

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -3,14 +3,25 @@ import { NextResponse } from "next/server";
 export const config = {
   matcher: "/admin/:path*",
 };
-
-const allowOrigins = ["www.poys.icu",];
+const protectedRoutes = ["/admin"];
+const allowOrigins = ["www.poys.icu"];
 
 export default async function middleware(request) {
+  let response = NextResponse.next();
+  response = AuthenticateHandler(request, response);
 
+  return response;
 }
 
-
 function AuthenticateHandler(request, response) {
-    
+  const isAdminRoute = protectedRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route)
+  );
+  const token = request.cookies.get("token")?.value;
+
+  if (isAdminRoute && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
+  return response;
 }
