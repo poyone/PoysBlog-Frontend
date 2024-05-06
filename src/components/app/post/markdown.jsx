645857e4695f35686344844435b06690c453fae0
poyone <86655640+poyone@users.noreply.github.com>
2024-05-07 01:08:51 +0800

    hotfix:highlight组件导入问题

--- src/components/app/post/markdown.jsx
+++ src/components/app/post/markdown.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
-import Highlight from "./Highlight";
+import Highlight from "./highlight";
 
 export default function MarkdownToHtml({ content }) {
   return (
